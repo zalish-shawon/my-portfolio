67a80cf775f2efca08aef2166a844216ac7cbfdb
zalish-shawon <zalish.shawon@gmail.com>
2023-12-26 01:16:45 +0600

    updated

--- src/components/Home/ContactMe.jsx
+++ src/components/Home/ContactMe.jsx
@@ -33,7 +33,7 @@ const ContactMe = () => {
                             <div class="lg:w-1/2 lg:mx-6 mt-8" data-aos="fade-right"
                                 data-aos-offset="300"
                                 data-aos-easing="ease-in-sine">
-                                <h1 class="text-2xl font-semibold text-gray-800 capitalize dark:text-white lg:text-3xl">
+                                <h1 class="text-2xl font-semibold  capitalize text-gray-300 lg:text-3xl">
                                     Contact me for <br /> more info
                                 </h1>
                                 <div class="mt-6 space-y-8 md:mt-8">
@@ -43,7 +43,7 @@ const ContactMe = () => {
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                                         </svg>
 
-                                        <span class="mx-2 text-gray-700 truncate w-72 dark:text-gray-400">
+                                        <span class="mx-2 text-gray-400 truncate w-72 ">
                                             Uttara Dhaka - 1230
                                         </span>
                                     </p>
@@ -53,7 +53,7 @@ const ContactMe = () => {
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                                         </svg>
 
-                                        <span class="mx-2 text-gray-700 truncate w-72 dark:text-gray-400">+8801784866101</span>
+                                        <span class="mx-2 text-gray-400 truncate w-72 ">+8801784866101</span>
                                     </p>
 
                                     <p class="flex items-start -mx-2">
@@ -61,17 +61,17 @@ const ContactMe = () => {
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                                         </svg>
 
-                                        <span class="mx-2 text-gray-700 truncate w-72 dark:text-gray-400">zalish.shawon@gmail.com</span>
+                                        <span class="mx-2 text-gray-400 truncate w-72 ">zalish.shawon@gmail.com</span>
                                     </p>
                                 </div>
 
                                 <div class="mt-6 w-80 md:mt-8">
-                                    <h3 class="text-gray-600 dark:text-gray-300 ">Follow me</h3>
+                                    <h3 class="text-gray-300 ">Follow me</h3>
 
                                     <div class="flex mt-4 -mx-1.5 ">
                                        
 
-                                        <a class="mx-1.5 dark:hover:text-blue-400 text-gray-400 transition-colors duration-300 transform hover:text-blue-500" href="https://www.linkedin.com/in/zalish-shawon/" target="_blank">
+                                        <a class="mx-1.5 hover:text-blue-400 text-gray-400 transition-colors duration-300 transform" href="https://www.linkedin.com/in/zalish-shawon/" target="_blank">
                                             <svg class="w-8 h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M15.2 8.80005C16.4731 8.80005 17.694 9.30576 18.5941 10.2059C19.4943 11.1061 20 12.327 20 13.6V19.2H16.8V13.6C16.8 13.1757 16.6315 12.7687 16.3314 12.4687C16.0313 12.1686 15.6244 12 15.2 12C14.7757 12 14.3687 12.1686 14.0687 12.4687C13.7686 12.7687 13.6 13.1757 13.6 13.6V19.2H10.4V13.6C10.4 12.327 10.9057 11.1061 11.8059 10.2059C12.7061 9.30576 13.927 8.80005 15.2 8.80005Z" fill="currentColor" />
                                                 <path d="M7.2 9.6001H4V19.2001H7.2V9.6001Z" fill="currentColor" />
@@ -79,7 +79,7 @@ const ContactMe = () => {
                                             </svg>
                                         </a>
 
-                                        <a class="mx-1.5 dark:hover:text-blue-400 text-gray-400 transition-colors duration-300 transform hover:text-blue-500" href="https://www.facebook.com/zalish.shawon" target="_blank">
+                                        <a class="mx-1.5 hover:text-blue-400 text-gray-400 transition-colors duration-300 transform" href="https://www.facebook.com/zalish.shawon" target="_blank">
                                             <svg class="w-8 h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M7 10.2222V13.7778H9.66667V20H13.2222V13.7778H15.8889L16.7778 10.2222H13.2222V8.44444C13.2222 8.2087 13.3159 7.9826 13.4826 7.81591C13.6493 7.64921 13.8754 7.55556 14.1111 7.55556H16.7778V4H14.1111C12.9324 4 11.8019 4.46825 10.9684 5.30175C10.1349 6.13524 9.66667 7.2657 9.66667 8.44444V10.2222H7Z" fill="currentColor" />
                                             </svg>
@@ -94,21 +94,21 @@ const ContactMe = () => {
                                 data-aos-offset="300"
                                 data-aos-easing="ease-in-sine">
                                 <div
-                                    class="w-full px-8 pb-10 mx-auto overflow-hidden bg-white rounded-lg shadow-2xl dark:bg-gray-900 lg:max-w-xl shadow-gray-300/50 dark:shadow-black/50">
+                                    class="w-full px-8 pb-10 mx-auto overflow-hidden rounded-lg shadow-2xl bg-gray-900 lg:max-w-xl shadow-black/50">
                                     <form ref={form} onSubmit={sendEmail} className="mt-6">
                                         <div class="flex-1">
-                                            <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Full Name</label>
-                                            <input name="user_name" type="text" placeholder="John Doe" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                                            <label class="block mb-2 text-sm text-gray-200">Full Name</label>
+                                            <input name="user_name" type="text" placeholder="John Doe" class="block w-full px-5 py-3 mt-2 text-gray-300 border rounded-md placeholder-gray-600 bg-gray-900 border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                                         </div>
 
                                         <div class="flex-1 mt-6">
-                                            <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email address</label>
-                                            <input name="user_email" type="email" placeholder="johndoe@example.com" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                                            <label class="block mb-2 text-sm text-gray-200">Email address</label>
+                                            <input name="user_email" type="text" placeholder="JohnDoe@email.com" class="block w-full px-5 py-3 mt-2 text-gray-300 border rounded-md placeholder-gray-600 bg-gray-900 border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                                         </div>
 
                                         <div class="w-full mt-6">
                                             <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Message</label>
-                                            <textarea name="message" class="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Message"></textarea>
+                                            <textarea name="message" class="block w-full h-32 px-5 py-3 mt-2 text-gray-300 border rounded-md md:h-48 placeholder-gray-600 bg-gray-900 border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Message"></textarea>
                                         </div>
 
                                         <button type="submit" class="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
